63fcdee217f4210defe29aecacff009ba8612b87
lushangkan <1149014559@qq.com>
2023-08-06 23:26:50 +0800

    Complete editor datepicker.

--- src/componrnts/editor/EditorDatepicker.vue
+++ src/componrnts/editor/EditorDatepicker.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="absolute left-0 top-[-35px] w-screen flex flex-row justify-center items-center">
-    <vue-date-picker class="w-[320px] editor-datepicker drop-shadow-[0_0_10px_hsla(var(--n)/0.15)]" inline auto-apply :close-on-auto-apply="false"
-                     :locale="useI18n().locale.value"
-                     :six-weeks="true" v-bind="$attrs"
-                     menu-class-name="datepicker-menu editor-datepicker-menu"
-                     :on-click-outside="$emit('on-click-outside')"
-    />
-  </div>
+  <Transition name="editor-datepicker">
+    <div class="absolute left-0 top-[-35px] w-screen flex flex-row justify-center items-center">
+      <div class="drop-shadow-[0_0_10px_hsla(var(--n)/0.15)]">
+        <vue-date-picker class="w-[320px] editor-datepicker" inline auto-apply :close-on-auto-apply="false"
+                         :locale="useI18n().locale.value"
+                         :six-weeks="true" v-bind="$attrs"
+                         menu-class-name="datepicker-menu editor-datepicker-menu"
+                         :transitions="{menuAppear: 'editor-datepicker' }"
+        />
+      </div>
+    </div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
 import {useI18n} from "vue-i18n";
-import {defineEmits, ref, watch} from "vue";
-import moment from "moment/moment";
 import VueDatePicker from "@vuepic/vue-datepicker";
 
 </script>
--- src/componrnts/editor/TaskEditor.vue
+++ src/componrnts/editor/TaskEditor.vue
@@ -1,47 +1,49 @@
 <template>
   <div class="fixed top-0 w-full h-full bg-black bg-opacity-10 flex flex-col justify-end items-center z-30">
     <div v-drag="{ axis: 'y', handle: '.guide-strip', snap:'100' }" @click="onClickEditor($event)" :class="`${detailMode?'editor-detail':''} absolute editor w-full rounded-t-[40px] bg-base-100 flex flex-col justify-around items-center py-[21px] gap-[20px]`">
-      <div class="top flex w-full flex-col justify-around items-center gap-[20px]">
-        <div class="guide-strip w-full h-[20px] flex flex-row justify-center items-center">
-          <div class="w-[50px] rounded-full border-base-300 border-[3px]"/>
-        </div>
-        <a class="text-stone-500 text-base font-light">新建任务</a>
-      </div>
+     <div id="editor-content">
+       <div class="top flex w-full flex-col justify-around items-center gap-[20px]">
+         <div class="guide-strip w-full h-[20px] flex flex-row justify-center items-center">
+           <div class="w-[50px] rounded-full border-base-300 border-[3px]"/>
+         </div>
+         <a class="text-stone-500 text-base font-light">新建任务</a>
+       </div>
 
-      <div class="line1 flex flex-row justify-around items-center gap-[15px]">
-        <div class="icon-color">
-          <button type="button" title="selecting colors and icons" class="d-btn d-btn-circle h-[55px] w-[55px] bg-warning border-base-300 border-[4px] border-solid flex flex-row justify-center items-center">
-            <Icon icon="noto:cookie" class="h-[35px] w-[35px]"/>
-          </button>
-        </div>
-        <div class="d-form-control">
-          <label v-if="detailMode" class="label pb-[3px] pl-[7px] flex flex-row justify-start items-center gap-[6px]">
-            <PenLine class="w-[15px] h-[15px] left-[13px] text-base-300" stroke-width="2.5px"/>
-            <a class="d-label-text text-neutral-content font-normal text-[13px]">任务名称</a>
-          </label>
-          <div class="input-group relative h-[50px] w-[193px]">
-            <input type="text" :placeholder="detailMode?'':'任务名称'" class="d-input d-input-bordered w-full h-full max-w-xs rounded-[18px] pl-[40px]">
-            <PenLine v-if="!detailMode" class="vertical-centering w-[20px] h-[20px] left-[13px] text-base-300"/>
-          </div>
-        </div>
-      </div>
-      <div v-if="detailMode" class="line2 w-[267px] flex flex-row justify-between items-center">
-        <div class="tag-picker d-form-control w-[47%]">
-          <label class="d-label pb-[3px] pl-[7px] flex flex-row justify-start items-center gap-[6px]">
-            <TagIcon class="w-[15px] h-[15px] left-[13px] text-base-300" stroke-width="2.5px"/>
-            <a class="label-text text-neutral-content font-normal text-[13px]">任务标签</a>
-          </label>
-          <tag-picker class="d-input d-input-bordered w-full h-[45px] max-w-xs rounded-[18px]"/>
-        </div>
-        <div class="date-picker d-form-control w-[47%]">
-          <label class="d-label pb-[3px] pl-[7px] flex flex-row justify-start items-center gap-[6px]">
-            <CalendarCheckIcon class="w-[15px] h-[15px] left-[13px] text-base-300" stroke-width="2.5px"/>
-            <a class="label-text text-neutral-content font-normal text-[13px]">完成期限</a>
-          </label>
-          <editor-datepicker v-if="datepickerOpen" v-model="dateValue"/>
-          <input ref="ddlInputRef" type="text" @click="datepickerOpen=!datepickerOpen" v-model="displayDate" readonly="readonly" placeholder="选择日期" class="ddl-input d-input d-input-bordered w-full h-[45px] max-w-xs rounded-[18px] text-[13px] font-light placeholder:text-base-300"/>
-        </div>
-        </div>
+       <div class="line1 flex flex-row justify-around items-center gap-[15px]">
+         <div class="icon-color">
+           <button type="button" title="selecting colors and icons" class="d-btn d-btn-circle h-[55px] w-[55px] bg-warning border-base-300 border-[4px] border-solid flex flex-row justify-center items-center">
+             <Icon icon="noto:cookie" class="h-[35px] w-[35px]"/>
+           </button>
+         </div>
+         <div class="d-form-control">
+           <label v-if="detailMode" class="label pb-[3px] pl-[7px] flex flex-row justify-start items-center gap-[6px]">
+             <PenLine class="w-[15px] h-[15px] left-[13px] text-base-300" stroke-width="2.5px"/>
+             <a class="d-label-text text-neutral-content font-normal text-[13px]">任务名称</a>
+           </label>
+           <div class="input-group relative h-[50px] w-[193px]">
+             <input type="text" :placeholder="detailMode?'':'任务名称'" class="d-input d-input-bordered w-full h-full max-w-xs rounded-[18px] pl-[40px]">
+             <PenLine v-if="!detailMode" class="vertical-centering w-[20px] h-[20px] left-[13px] text-base-300"/>
+           </div>
+         </div>
+       </div>
+       <div v-if="detailMode" class="line2 w-[267px] flex flex-row justify-between items-center">
+         <div class="tag-picker d-form-control w-[47%]">
+           <label class="d-label pb-[3px] pl-[7px] flex flex-row justify-start items-center gap-[6px]">
+             <TagIcon class="w-[15px] h-[15px] left-[13px] text-base-300" stroke-width="2.5px"/>
+             <a class="label-text text-neutral-content font-normal text-[13px]">任务标签</a>
+           </label>
+           <tag-picker class="d-input d-input-bordered w-full h-[45px] max-w-xs rounded-[18px]"/>
+         </div>
+         <div class="date-picker d-form-control w-[47%]">
+           <label class="d-label pb-[3px] pl-[7px] flex flex-row justify-start items-center gap-[6px]">
+             <CalendarCheckIcon class="w-[15px] h-[15px] left-[13px] text-base-300" stroke-width="2.5px"/>
+             <a class="label-text text-neutral-content font-normal text-[13px]">完成期限</a>
+           </label>
+           <editor-datepicker v-if="datepickerOpen" v-model="dateValue"/>
+           <input ref="ddlInputRef" type="text" @click="datepickerOpen=!datepickerOpen" v-model="displayDate" readonly="readonly" placeholder="选择日期" class="ddl-input d-input d-input-bordered w-full h-[45px] max-w-xs rounded-[18px] text-[13px] font-light placeholder:text-base-300"/>
+         </div>
+       </div>
+     </div>
       <div class="flex flex-row justify-end items-center w-[268px]">
         <button type="button" title="continue" class="d-btn d-btn-circle h-[55px] w-[55px] bg-primary flex flex-row justify-center items-center">
           <check class="text-base-100 h-[28px] w-[28px]"></check>
