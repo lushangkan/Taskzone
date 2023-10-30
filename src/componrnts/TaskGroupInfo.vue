750e2dd6b629bc285b9f9e7bccfd8c9e3c217487
lushangkan <1149014559@qq.com>
2023-10-30 20:54:44 +0800

    Automatically switch the text color according to the TaskGroupInfo background color.

--- src/componrnts/TaskGroupInfo.vue
+++ src/componrnts/TaskGroupInfo.vue
@@ -1,14 +1,19 @@
 <template>
-  <div ref="infoCardRef" class="w-full min-h-[180px] rounded-[22px] flex flex-col justify-start items-center px-[26px] py-[16px] gap-[18px]" :style="{ '--group-color': groupColor === undefined? 'var(--p)' : groupColor, backgroundColor: 'hsla(var(--group-color) / 0.15)' }">
+  <div ref="infoCardRef" class="w-full min-h-[180px] rounded-[22px] flex flex-col justify-start items-center px-[26px] py-[16px] gap-[18px]" :style="
+  {
+    '--group-color': groupColor === undefined? 'var(--p)' : groupColor,
+    backgroundColor: 'hsla(var(--group-color) / 0.15)',
+    '--fg': `var(${fgColor})`
+  }">
     <div class="w-full flex flex-col justify-start items-center gap-[7px]">
       <div class="w-full flex flex-row justify-between items-center">
-        <span class="text-neutral text-[17px] font-medium">{{ props.taskGroupEntity.name }}</span>
+        <span class="text-[hsl(var(--fg))] text-[17px] font-medium">{{ props.taskGroupEntity.name }}</span>
         <div class="h-[42px] w-[42px] rounded-[13px] flex justify-center items-center bg-base-100 task-icon-bg-shadow">
           <span class="text-[24px]">{{ props.taskGroupEntity?.icon }}</span>
         </div>
       </div>
       <div v-if="props.taskGroupEntity?.description !== null && props.taskGroupEntity?.description.trim() !== ''" class="w-full flex flex-row justify-start items-center">
-        <span class="text-neutral/80 font-light text-[14.5px]">{{ props.taskGroupEntity.description }}</span>
+        <span class="text-[hsl(var(--fg))]/80 font-light text-[14.5px]">{{ props.taskGroupEntity.description }}</span>
       </div>
     </div>
     <overlay-scrollbars-component v-if="props.taskGroupEntity?.tags.length !== 0" defer element="div" :options="{
@@ -33,26 +38,26 @@
     <div class="w-full flex flex-row justify-between items-end pb-[5.3px]">
       <div class="h-[55px] w-full pb-[3px] flex flex-col flex-wrap justify-between items-start gap-y-[8px] gap-x-[13px] overflow-hidden">
         <div v-if="props.taskGroupEntity !== undefined && props.taskGroupEntity.deadLineDate !== null"
-             class="max-w-[100px] flex flex-row justify-start items-center gap-[12px] overflow-hidden">
-          <alarm-check-icon class="h-[19px] w-[19px] stroke-neutral/70 stroke-[2px]"/>
-          <span class="font-light text-[13.5px] text-clip overflow-hidden whitespace-nowrap">{{ fun.getShortDate(props.taskGroupEntity.deadLineDate) }}</span>
+             class="h-[21px] max-w-[100px] flex flex-row justify-start items-center gap-[12px] overflow-hidden">
+          <alarm-check-icon class="h-[19px] w-[19px] stroke-[hsl(var(--fg))]/70 stroke-[2px]"/>
+          <span class="text-[hsl(var(--fg))] font-light text-[13.5px] text-clip overflow-hidden whitespace-nowrap">{{ fun.getShortDate(props.taskGroupEntity.deadLineDate) }}</span>
         </div>
         <div v-if="props.taskGroupEntity !== undefined && props.taskGroupEntity.repeatMode !== RepeatMode.ONLY_ONCE"
-             class="flex flex-row justify-start items-center gap-[12px] overflow-hidden">
-          <repeat-icon class="h-[19px] w-[19px] stroke-neutral/70 stroke-[2px]"/>
-          <span class="font-light text-[13.5px] text-clip overflow-hidden whitespace-nowrap">{{ $t(`repeatMode.${props.taskGroupEntity.repeatMode}`) }}</span>
+             class="h-[21px] flex flex-row justify-start items-center gap-[12px] overflow-hidden">
+          <repeat-icon class="h-[19px] w-[19px] stroke-[hsl(var(--fg))]/70 stroke-[2px]"/>
+          <span class="text-[hsl(var(--fg))] font-light text-[13.5px] text-clip overflow-hidden whitespace-nowrap">{{ $t(`repeatMode.${props.taskGroupEntity.repeatMode}`) }}</span>
         </div>
         <div v-if="props.taskGroupEntity !== undefined && props.taskGroupEntity.priority !== Priority.NORMAL"
-             class="flex flex-row justify-start items-center gap-[12px] overflow-hidden">
-          <alert-circle-icon class="h-[19px] w-[19px] stroke-neutral/70 stroke-[2px]"/>
+             class="h-[21px] flex flex-row justify-start items-center gap-[12px] overflow-visible">
+          <alert-circle-icon class="h-[19px] w-[19px] stroke-[hsl(var(--fg))]/70 stroke-[2px]"/>
           <!-- TODO:根据背景颜色和文字颜色判断是否要添加背景 -->
-          <span :class="`${priorityColor[props.taskGroupEntity.priority] === undefined? 'font-light':'font-bold'} text-[13.5px] text-clip overflow-hidden whitespace-nowrap`" :style="{ color: `var(${priorityColor[props.taskGroupEntity.priority]})` }">{{ $t(`priority.${props.taskGroupEntity.priority}`) }}</span>
+          <span :class="`${priorityColors[props.taskGroupEntity.priority] === undefined? 'font-light':'font-bold'} ${priorityBgColor !== undefined? 'rounded-full border-[5px]':''} text-[13.5px] text-center text-clip overflow-hidden whitespace-nowrap text-[hsl(var(--fg))]`" :style="{ color: `var(${priorityColors[props.taskGroupEntity.priority]})`, borderColor: `hsl(var(${priorityBgColor}))`, backgroundColor: `hsl(var(${priorityBgColor}))` }">{{ $t(`priority.${props.taskGroupEntity.priority}`) }}</span>
         </div>
         <div v-if="props.taskGroupEntity !== undefined && props.taskGroupEntity.repeatMode !== RepeatMode.ONLY_ONCE"
-             class="flex flex-row justify-start items-center gap-[12px] overflow-hidden">
-          <bell-icon class="h-[19px] w-[19px] stroke-neutral/70 stroke-[2px]"/>
+             class="h-[21px] flex flex-row justify-start items-center gap-[12px] overflow-hidden">
+          <bell-icon class="h-[19px] w-[19px] stroke-[hsl(var(--fg))]/70 stroke-[2px]"/>
           <!-- TODO:根据背景颜色和文字颜色判断是否要添加背景 -->
-          <span class="font-light text-[13.5px] whitespace-nowrap">{{ $t(`reminderMode.${props.taskGroupEntity.reminders}`) }}</span>
+          <span class="text-[hsl(var(--fg))] font-light text-[13.5px] whitespace-nowrap">{{ $t(`reminderMode.${props.taskGroupEntity.reminders}`) }}</span>
         </div>
       </div>
     </div>
@@ -81,6 +86,9 @@ const props = defineProps({
 });
 
 const groupColor: Ref<string | undefined> = ref(undefined);
+const fgColor: Ref<string> = ref('--n');
+const priorityBgColor: Ref<string | undefined> = ref(undefined);
+
 const infoCardRef: Ref<HTMLDivElement | null> = ref(null);
 let scrollEle: HTMLElement | null | undefined = null;
 
@@ -88,7 +96,7 @@ const appStore = useAppStores();
 
 const router = useRouter();
 
-const priorityColor = reactive({
+const priorityColors = reactive({
   [Priority.LOW]: undefined,
   [Priority.NORMAL]: undefined,
   [Priority.MEDIUM]: '--oc-yellow-6',
@@ -117,11 +125,33 @@ const onScroll = () => {
   }
 }
 
-onMounted(() => {
+function initColorVar() {
   if (typeof props.taskGroupEntity?.color === 'string') {
     const color = new Color(props.taskGroupEntity.color);
     groupColor.value = color.to('hsl', {}).toString().replace('hsl(', '').replace(')', '');
   }
+
+  const b1Color: Color = new Color(`hsl(${fun.getCssVar('--b1')})`);
+  const bgColor: Color = new Color(`hsla(${groupColor.value}, 0.15)`)
+
+  const mixColor: Color = b1Color.mix(bgColor, undefined, undefined) as Color;
+
+  fgColor.value = fun.getForegroundColor(mixColor.to('srgb', {}).toString());
+
+  if (props.taskGroupEntity?.priority !== Priority.NORMAL && props.taskGroupEntity?.priority !== Priority.LOW) {
+    const priorityColor = new Color(`hsl(${fun.getCssVar(priorityColors[props.taskGroupEntity.priority]!)})`);
+
+    const contrast = mixColor.contrastAPCA(priorityColor);
+
+    if (contrast > -40) {
+      // 优先级颜色和背景颜色对比度不够，需要添加背景
+      priorityBgColor.value = fgColor.value === '--n'? '--b1' : fgColor.value;
+    }
+  }
+}
+
+onMounted(() => {
+  initColorVar();
 
   // 获取滚动元素
   initScroll();
